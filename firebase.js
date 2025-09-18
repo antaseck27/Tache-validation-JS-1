e913134fdc5683619958c00f0fa6e522fafd832c
antaseck27 <antalissa10@gmail.com>
2025-09-18 16:00:36 +0000

    test

---
+++ firebase.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDXbPOoyQ16Kli9I7G7jOXj8GSXQRWUl5Y",
+    authDomain: "tache-valid-js.firebaseapp.com",
+    projectId: "tache-valid-js",
+    storageBucket: "tache-valid-js.firebasestorage.app",
+    messagingSenderId: "879828418362",
+    appId: "1:879828418362:web:fe2086b0508ad03974da03"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
