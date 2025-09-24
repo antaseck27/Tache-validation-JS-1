cf1db0e8712190d3820d48819f5198a43d10f251
IG-gning <idrissagning826@gmail.com>
2025-09-24 13:24:43 +0000

    add modification

--- inscrire.js
+++ inscrire.js
@@ -1,13 +1,13 @@
 // je declare les input
-let inputNom = document.getElementById("inputNom").valus;
-let inputNaissance = document.getElementById("inputNaissance").valus;
-let inputDmot2passe = document.getElementById("inputDmot2passe").valus;
-let inputMail = document.getElementById("inputMail").valus;
-let inputAdresse = document.getElementById("inputAdresse").valus;
-let inputPrenom = document.getElementById("inputPrenom").valus;
-let inputProfession = document.getElementById("inputProfession").valus;
-let inputTelephone = document.getElementById("inputTelephone").valus;
-let inputOrganisation = document.getElementById("inputOrganisation").valus;
+let inputNom = document.getElementById("inputNom").value;
+let inputNaissance = document.getElementById("inputNaissance").value;
+let inputDmot2passe = document.getElementById("inputDmot2passe").value;
+let inputMail = document.getElementById("inputMail").value;
+let inputAdresse = document.getElementById("inputAdresse").value;
+let inputPrenom = document.getElementById("inputPrenom").value;
+let inputProfession = document.getElementById("inputProfession").value;
+let inputTelephone = document.getElementById("inputTelephone").value;
+let inputOrganisation = document.getElementById("inputOrganisation").value;
 
 // je declare mon bouton s'inscrire
 let inscrire = document.getElementById("inscrire");
